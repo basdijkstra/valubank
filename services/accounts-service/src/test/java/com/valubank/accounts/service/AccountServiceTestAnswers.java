312f53f60ed93cadb249735230df93918caac45e
Bas Dijkstra <bas@ontestautomation.com>
2026-09-07 13:39:39 +0200

    Add PITest to pom.xml

--- services/accounts-service/src/test/java/com/valubank/accounts/service/AccountServiceTestAnswers.java
+++ services/accounts-service/src/test/java/com/valubank/accounts/service/AccountServiceTestAnswers.java
@@ -21,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(MockitoExtension.class) 
 class AccountServiceTestAnswers {
 
     // Cross process/IO boundaries - mocked so the test is fast and deterministic,
